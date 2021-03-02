c679b50ef7502e26b29c000007974e47e0fc4068
Sara-n7 <79912178+Sara-n7@users.noreply.github.com>
2021-03-02 12:39:50 +0100

    Add files via upload

---
+++ Tarea_4/src/cuentas/CCuenta.java
@@ -0,0 +1,70 @@
+package cuentas;
+
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    public double estado()
+    {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(saldo + cantidad);
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(saldo - cantidad);
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterÃ
+     */
+    public double getTipoInterÃ() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterÃ the tipoInterÃ to set
+     */
+    public void setTipoInterÃ(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+}
